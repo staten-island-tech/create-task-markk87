534e93e9b04b096553fe77dfa7448340189b46fb
markk87 <markk87@nycstudents.net>
2024-01-11 09:23:13 -0500

    sdfgsdfg

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
+
+
+async function getActivity(){
+    const URL = `https://www.boredapi.com/api/activity`
+    try {
+        const response = await fetch(URL);
+        console.log(response);
+        const data = await response.json();
+        console.log(data);
+        if (data.result === "success") {
+            DOMSelectors.button.addEventListener("submit", function(event) {
+                event.preventDefault();
+            
+                
+                
+                inputData();
+            });
+        }
+
+
+        function inputData(data) {
+            const randomActivity = data.activity
+            DOMSelectors.gallery.insertAdjacentHTML("beforeend", `<div class=card><button class=remove>Clear</button><h2> ${DOMSelectors.nameInput.value} : ${DOMSelectors.emailInput.value} : ${DOMSelectors.ageInput.value}</h2></div>`)
+           }
+    } catch (error) {
+    console.error("Not yet")
+}}
+
+
+
+
+
 const DOMSelectors = {
     form: document.querySelector("#form"),
     gallery: document.querySelector(".gallery"),
@@ -7,20 +39,7 @@ const DOMSelectors = {
     ageInput: document.querySelector(".ageInput")
 };
 
-DOMSelectors.button.addEventListener("click", function(event) {
-    event.preventDefault();
-
-    function inputData() {
-        DOMSelectors.gallery.insertAdjacentHTML("beforeend", `<div class=card><button class=remove>Clear</button><h2> ${DOMSelectors.nameInput.value} : ${DOMSelectors.emailInput.value} : ${DOMSelectors.ageInput.value}</h2></div>`)
-    }
-    
-    inputData();
-});
 
 
-
-for(let i = 0; i < DOMSelectors.nameInput.length; i++ ) {
-    console.log(i)
-}
 
 
